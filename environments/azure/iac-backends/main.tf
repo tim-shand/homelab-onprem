3e4db31dc2f19b1be4e534b28c43ac053a7f396d
Tim Shand <tim@tshand.com>
2025-11-10 18:15:08 +1300

    feat: Adding backends for Azure and Proxmox resources.

--- environments/azure/iac-backends/main.tf
+++ environments/azure/iac-backends/main.tf
@@ -2,9 +2,36 @@
 # Azure IaC Backends: Variables
 #=================================================================#
 
-module "iac_backend" {
+#-------------------#
+# Azure
+#-------------------#
+
+# Backend: Azure - Platform Landing Zone
+module "iac_backend_plz" {
   source = "../../../modules/az_iac-backend"
   iac_container_name = "tfstate-azure-platformlz"
+  subscription_id_iac = var.subscription_id_iac
+  iac_sa_name = var.iac_sa_name
+  iac_sa_rg = var.iac_sa_rg
+}
+
+# Backend: Azure - Workload: Personal Website
+module "iac_backend_workload_wwwtshandcom" {
+  source = "../../../modules/az_iac-backend"
+  iac_container_name = "tfstate-azure-wwwtshandcom"
+  subscription_id_iac = var.subscription_id_iac
+  iac_sa_name = var.iac_sa_name
+  iac_sa_rg = var.iac_sa_rg
+}
+
+#-------------------#
+# Proxmox
+#-------------------#
+
+# Backend: Proxmox
+module "iac_backend_proxmox_core" {
+  source = "../../../modules/az_iac-backend"
+  iac_container_name = "tfstate-proxmox-core"
   subscription_id_iac = var.subscription_id_iac
   iac_sa_name = var.iac_sa_name
   iac_sa_rg = var.iac_sa_rg
